a5c7941e1a0d57f004c2719be4eced6be3e4770a
FlyDragon1101 <112711706+NoahNg422@users.noreply.github.com>
2025-03-03 13:59:24 -0600

    Update loading_1.gd

--- new-game-project/Scripts/loading_1.gd
+++ new-game-project/Scripts/loading_1.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 
 func _input(button):
 	if button.is_action_pressed("ui_select"):
-		get_tree().change_scene_to_file("res://Menu0.tscn")
+		get_tree().change_scene_to_file("res://scripts//Menu0.tscn")
 	if Input.is_action_just_pressed("Fullscreen") and full:
 		full = not full
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
